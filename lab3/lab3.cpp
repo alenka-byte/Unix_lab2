f9a23f0cff0d530696d37e7391b29e7f37fb3fbb
alenka-byte <alenkaivas6@gmail.com>
2025-11-14 16:19:59 +0700

    Update lab3.cpp

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -42,32 +42,21 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     unordered_map<string, fs::path> hash_to_file;
-    try {
-        for (const auto& entry : fs::recursive_directory_iterator(directory)) {
-            if (fs::is_regular_file(entry)) {
-                try {
-                    string hash = computeSHA1(entry.path());
-                    if (hash_to_file.find(hash) == hash_to_file.end()) {
-                        hash_to_file[hash] = entry.path();
-                        cout << "Original: " << entry.path() << " ["<<hash << "]" << endl;
-                    }
-                    else {
-                        fs::path original_file = hash_to_file[hash];
-                        fs::path current_file = entry.path();
-                        fs::remove(current_file);
-                        fs::create_hard_link(original_file, current_file);
-                        cout << "Replaced with hard link: " << current_file << " -> " << original_file << endl;
-                    }
-                }
-                catch (const exception& e) {
-                    cout << "Error processing " << entry.path() << ": " << e.what() << endl;
-                }
+    for (const auto& entry : fs::recursive_directory_iterator(directory)) {
+        if (fs::is_regular_file(entry)) {
+            string hash = computeSHA1(entry.path());
+            if (hash_to_file.find(hash) == hash_to_file.end()) {
+                hash_to_file[hash] = entry.path();
+                cout << "Original: " << entry.path() << " ["<<hash << "]" << endl;
             }
-        }
-    }
-    catch (const fs::filesystem_error& e) {
-        cout << "Filesystem error: " << e.what() << endl;
-        return 1;
+            else {
+                fs::path original_file = hash_to_file[hash];
+                fs::path current_file = entry.path();
+                fs::remove(current_file);
+                fs::create_hard_link(original_file, current_file);
+                cout << "Replaced with hard link: " << current_file << " -> " << original_file << endl;
+            }
+       }
     }
     cout << "Processing completed. " << hash_to_file.size() << " unique files found." << endl;
     return 0;
