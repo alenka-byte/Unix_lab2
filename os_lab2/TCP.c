7a59fddaa77f12511a12469c67998a62a413f01d
alenka-byte <alenkaivas6@gmail.com>
2025-11-27 16:50:26 +0700

    Update TCP.c

--- os_lab2/TCP.c
+++ os_lab2/TCP.c
@@ -1,80 +1,88 @@
-#include <vector>       
-#include <signal.h>     
-#include <sys/select.h> 
-#include <sys/types.h>  
-#include <sys/socket.h> 
-#include <netinet/in.h> 
-#include <unistd.h>     
-#include <errno.h>      
-#include <stdio.h>      
-#include <stdlib.h>     
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/select.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
 
+#define PORT 12345
+#define MAX_CLIENTS 1
 
-// Объявление обработчика сигнала
 volatile sig_atomic_t wasSigHup = 0;
-void sigHupHandler(int r)
-{
-	wasSigHup = 1;
+
+void sigHupHandler(int sig) {
+    wasSigHup = 1;
 }
 
 int main() {
-	// Регистрация обработчика сигнала
-	struct sigaction sa;
-	sigaction(SIGHUP, NULL, &sa);
-	sa.sa_handler = sigHupHandler;
-	sa.sa_flags |= SA_RESTART;
-	sigaction(SIGHUP, &sa, NULL);
+    int serverSocket, clientSocket;
+    // Создание сокета
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
 
+    // Настройка адреса сервера
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddr.sin_port = htons(PORT);
 
-	// Блокировка сигнала
-	sigset_t blockedMask, origMask;
-	sigemptyset(&blockedMask);
-	sigaddset(&blockedMask, SIGHUP);
-	sigprocmask(SIG_BLOCK, &blockedMask, &origMask);
+    // Привязка сокета к адресу
+    if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+        perror("ошибка bind");
+        close(serverSocket);
+        exit(EXIT_FAILURE);
+    }
 
+    // Прослушивание сокета
+    if (listen(serverSocket, MAX_CLIENTS) < 0) {
+        perror("ошибка listen");
+        close(serverSocket);
+        exit(EXIT_FAILURE);
+    }
+    printf("сервер запущен на порту 12345. Ожидание соединений\n");
 
-	// сокет
-	int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    // Регистрация обработчика сигнала
+    struct sigaction sa;
+    sigaction(SIGHUP, NULL, &sa);
+    sa.sa_handler = sigHupHandler;
+    sa.sa_flags |= SA_RESTART;
+    sigaction(SIGHUP, &sa, NULL);
 
-	// Привязка к порту
-	struct sockaddr_in serverAddr;
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serverAddr.sin_port = htons(1255);  
+    // Блокировка сигнала
+    sigset_t blockedMask, origMask;
+    sigemptyset(&blockedMask);
+    sigaddset(&blockedMask, SIGHUP);
+    sigprocmask(SIG_BLOCK, &blockedMask, &origMask);
 
-	if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-		perror("Ошибка bind");
-		exit(EXIT_FAILURE);
-	}
+    // Основной цикл приложения
+    fd_set fds;
+    int maxFd=serverSocket;
+    while (true) {
+        FD_ZERO(&fds);
+        FD_SET(serverSocket, &fds);
+        maxFd = serverSocket;
 
-	if (listen(serverSocket, SOMAXCONN) < 0) {
-		perror("Ошибка listen");
-		exit(EXIT_FAILURE);
-	}
-
-	printf("Сервер запущен на порту 1255. Ожидание соединений...\n");
-
-	// Работа основного цикла
-	std::vector<int> clients;
-	int maxFd = serverSocket;
-	fd_set fds; // Переменная для хранения всех дескрипторов
-
-	while (true)
-	{
-		FD_ZERO(&fds);	// сбрасываем каждый раз обязательно
-		FD_SET(serverSocket, &fds);
-		maxFd = serverSocket;
-
-		for (int clientSocket : clients) {
+        // Добавление клиентского сокета, если он есть
+        if (clientSocket > 0) {
 			FD_SET(clientSocket, &fds);
-			if (clientSocket > maxFd) maxFd = clientSocket;
-		}
+            if (clientSocket > maxFd)
+                maxFd = clientSocket;
+        }
 
-		if (pselect(maxFd + 1, &fds, NULL, NULL, NULL, &origMask) == -1)
-		{
-			if (errno == EINTR)
+        // Вызов pselect()
+        int ready = pselect(maxFd + 1, &fds, NULL, NULL, NULL, &origMask);
+        if (ready == -1) {
+            if (errno == EINTR)
 			{
-				printf("Получен сигнал SIGHUP\n");
+				printf("Получен сигнал\n");
 				if (wasSigHup) {
 					wasSigHup = 0;
 					printf("Обработан сигнал SIGHUP\n");
@@ -85,47 +93,35 @@ int main() {
 			{
 				break;
 			}
-		}
-
-		// some actions on the descriptor activity
-		if (FD_ISSET(serverSocket, &fds)) {
-			int clientSocket = accept(serverSocket, NULL, NULL);
-			if (clientSocket != -1) {
+        }
+     
+            // Обработка нового соединения
+            if (FD_ISSET(serverSocket, &fds)) {
+                clientSocket = accept(serverSocket, NULL, NULL);
+               if (clientSocket != -1) {
 				printf("Новое соединение: %d\n", clientSocket);
+               }
+            }
 
-				// Оставляем одно соединение, остальные закрываем
-				for (int oldSocket : clients) {
-					printf("Оставляем одно соединение. Закрываем %d\n", oldSocket);
-					close(oldSocket);
-				}
-				clients.clear();
-				clients.push_back(clientSocket);
-			}
-		}
+            // Обработка данных от клиента
+            if (clientSocket > 0 && FD_ISSET(clientSocket, &fds)) {
+                char buffer[1024];
+                ssize_t bytesRead = read(clientSocket, buffer, sizeof(buffer));
+                if (bytesRead > 0) {
+                    printf("получил %zd байтов\n", bytesRead);
+                } else if (bytesRead == 0) {
+                    printf("Client disconnected\n");
+                    close(clientSocket);
+                    clientSocket = -1;
+                } else {
+                    perror("read");
+                    close(clientSocket);
+                    clientSocket = -1;
+                }
+            }
+        }
+    
 
-		// for the main socket and for every established connection
-		for (auto clientIt = clients.begin(); clientIt != clients.end();) {
-			int clientSocket = *clientIt;
-			if (FD_ISSET(clientSocket, &fds)) {
-				char buffer[1024];
-				ssize_t bytesRead = read(clientSocket, buffer, sizeof(buffer));
-				if (bytesRead <= 0) {
-					printf("Соединение %d закрыто.\n", clientSocket);
-					close(clientSocket);
-					clientIt = clients.erase(clientIt);
-				}
-				else {
-					printf("%zd байт от %d\n", bytesRead, clientSocket);
-					++clientIt;
-				}
-			}
-			else {
-				++clientIt;
-			}
-		}
-
-	}
-
-	close(serverSocket);
-	return 0;
+    close(serverSocket);
+    return 0;
 }
