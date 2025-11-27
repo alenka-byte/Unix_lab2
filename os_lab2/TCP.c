524d7059d4dbfc9e4fcc97437ce9c655816880b9
alenka-byte <alenkaivas6@gmail.com>
2025-11-27 16:43:33 +0700

    Update TCP.c

--- os_lab2/TCP.c
+++ os_lab2/TCP.c
@@ -1,158 +1,131 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <signal.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <errno.h>
-#include <sys/select.h>
+#include <vector>       
+#include <signal.h>     
+#include <sys/select.h> 
+#include <sys/types.h>  
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <unistd.h>     
+#include <errno.h>      
+#include <stdio.h>      
+#include <stdlib.h>     
 
-#define PORT 8080
-#define MAX_CLIENTS 10
 
+// Объявление обработчика сигнала
 volatile sig_atomic_t wasSigHup = 0;
-
-void sigHupHandler(int sig) {
-    wasSigHup = 1;
+void sigHupHandler(int r)
+{
+	wasSigHup = 1;
 }
 
 int main() {
-    int server_fd, new_socket, max_fd;
-    int client_sockets[MAX_CLIENTS] = {0};
-    fd_set fds;
-    struct sigaction sa;
-    sigset_t blockedMask, origMask;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
+	// Регистрация обработчика сигнала
+	struct sigaction sa;
+	sigaction(SIGHUP, NULL, &sa);
+	sa.sa_handler = sigHupHandler;
+	sa.sa_flags |= SA_RESTART;
+	sigaction(SIGHUP, &sa, NULL);
 
-    // Создание сокета
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
 
-    // Настройка адреса
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+	// Блокировка сигнала
+	sigset_t blockedMask, origMask;
+	sigemptyset(&blockedMask);
+	sigaddset(&blockedMask, SIGHUP);
+	sigprocmask(SIG_BLOCK, &blockedMask, &origMask);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
 
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
+	// сокет
+	int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    printf("Server listening on port %d\n", PORT);
+	// Привязка к порту
+	struct sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddr.sin_port = htons(1255);  
 
-    // Настройка обработчика сигналов
-    sigaction(SIGHUP, NULL, &sa);
-    sa.sa_handler = sigHupHandler;
-    sa.sa_flags |= SA_RESTART;
-    sigaction(SIGHUP, &sa, NULL);
+	if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+		perror("Ошибка bind");
+		exit(EXIT_FAILURE);
+	}
 
-    // Блокировка сигнала
-    sigemptyset(&blockedMask);
-    sigaddset(&blockedMask, SIGHUP);
-    sigprocmask(SIG_BLOCK, &blockedMask, &origMask);
-    
-    while (1) {
-        FD_ZERO(&fds);
-        FD_SET(server_fd, &fds);
-        max_fd = server_fd;
-        
-        // Добавление клиентских сокетов в набор
-        for (int i = 0; i < MAX_CLIENTS; i++) {
-            if (client_sockets[i] > 0) {
-                FD_SET(client_sockets[i], &fds);
-                if (client_sockets[i] > max_fd) {
-                    max_fd = client_sockets[i];
-                }
-            }
-        }
+	if (listen(serverSocket, SOMAXCONN) < 0) {
+		perror("Ошибка listen");
+		exit(EXIT_FAILURE);
+	}
 
-        // Безопасный вызов pselect 
-        if (pselect(max_fd + 1, &fds, NULL, NULL, NULL, &origMask) == -1) {
-            if (errno == EINTR) {
-                if (wasSigHup) {
-                    printf("Received SIGHUP signal\n");
-                    wasSigHup = 0;
-                }
-                continue;
-            } else {
-                perror("pselect error");
-                break;
-            }
-        }
+	printf("Сервер запущен на порту 1255. Ожидание соединений...\n");
 
-        // Проверка флага сигнала 
-        if (wasSigHup) {
-            printf("Received SIGHUP signal\n");
-            wasSigHup = 0;
-        }
+	// Работа основного цикла
+	std::vector<int> clients;
+	int maxFd = serverSocket;
+	fd_set fds; // Переменная для хранения всех дескрипторов
 
-        // Обработка нового подключения на основном сокете
-        if (FD_ISSET(server_fd, &fds)) {
-            if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-                                   (socklen_t*)&addrlen)) < 0) {
-                perror("accept");
-                continue;
-            }
+	while (true)
+	{
+		FD_ZERO(&fds);	// сбрасываем каждый раз обязательно
+		FD_SET(serverSocket, &fds);
+		maxFd = serverSocket;
 
-            printf("New connection accepted\n");
+		for (int clientSocket : clients) {
+			FD_SET(clientSocket, &fds);
+			if (clientSocket > maxFd) maxFd = clientSocket;
+		}
 
-            // Закрытие всех существующих соединений
-            for (int i = 0; i < MAX_CLIENTS; i++) {
-                if (client_sockets[i] > 0) {
-                    close(client_sockets[i]);
-                    client_sockets[i] = 0;
-                    printf("Closed existing connection\n");
-                }
-            }
+		if (pselect(maxFd + 1, &fds, NULL, NULL, NULL, &origMask) == -1)
+		{
+			if (errno == EINTR)
+			{
+				printf("Получен сигнал SIGHUP\n");
+				if (wasSigHup) {
+					wasSigHup = 0;
+					printf("Обработан сигнал SIGHUP\n");
+				}
+				continue;
+			}
+			else 
+			{
+				break;
+			}
+		}
 
-            // Добавление нового клиента
-            for (int i = 0; i < MAX_CLIENTS; i++) {
-                if (client_sockets[i] == 0) {
-                    client_sockets[i] = new_socket;
-                    break;
-                }
-            }
-        }
+		// some actions on the descriptor activity
+		if (FD_ISSET(serverSocket, &fds)) {
+			int clientSocket = accept(serverSocket, NULL, NULL);
+			if (clientSocket != -1) {
+				printf("Новое соединение: %d\n", clientSocket);
 
-        // Обработка данных от установленных соединений
-        for (int i = 0; i < MAX_CLIENTS; i++) {
-            int fd = client_sockets[i];
-            if (fd > 0 && FD_ISSET(fd, &fds)) {
-                char buffer[1024] = {0};
-                int valread = read(fd, buffer, sizeof(buffer));
-                if (valread > 0) {
-                    printf("Received %d bytes from client\n", valread);
-                } else if (valread == 0) {
-                    // Соединение закрыто клиентом
-                    close(fd);
-                    client_sockets[i] = 0;
-                    printf("Client disconnected\n");
-                } else {
-                    perror("read error");
-                    close(fd);
-                    client_sockets[i] = 0;
-                }
-            }
-        }
-    }
+				// Оставляем одно соединение, остальные закрываем
+				for (int oldSocket : clients) {
+					printf("Оставляем одно соединение. Закрываем %d\n", oldSocket);
+					close(oldSocket);
+				}
+				clients.clear();
+				clients.push_back(clientSocket);
+			}
+		}
 
-    // Очистка
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (client_sockets[i] > 0) {
-            close(client_sockets[i]);
-        }
-    }
-    close(server_fd);
-    return 0;
+		// for the main socket and for every established connection
+		for (auto clientIt = clients.begin(); clientIt != clients.end();) {
+			int clientSocket = *clientIt;
+			if (FD_ISSET(clientSocket, &fds)) {
+				char buffer[1024];
+				ssize_t bytesRead = read(clientSocket, buffer, sizeof(buffer));
+				if (bytesRead <= 0) {
+					printf("Соединение %d закрыто.\n", clientSocket);
+					close(clientSocket);
+					clientIt = clients.erase(clientIt);
+				}
+				else {
+					printf("%zd байт от %d\n", bytesRead, clientSocket);
+					++clientIt;
+				}
+			}
+			else {
+				++clientIt;
+			}
+		}
+
+	}
+
+	close(serverSocket);
+	return 0;
 }
